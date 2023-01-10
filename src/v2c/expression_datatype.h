aa45641768394e675a56082dd634f9d2a3e24d4e
dengxi <deng_xi@foxmail.com>
2023-01-10 22:13:10 +0800

    修改部分注释

--- src/v2c/expression_datatype.h
+++ src/v2c/expression_datatype.h
@@ -1,11 +1,3 @@
-/*******************************************************************\
-
-Module:
-
-Author: Daniel Kroening, kroening@kroening.com
-
-\*******************************************************************/
-
 #ifndef CPROVER_EXPRESSION_DATATYPE_H
 #define CPROVER_EXPRESSION_DATATYPE_H
 
